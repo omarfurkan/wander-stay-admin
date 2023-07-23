c2daf6901be8906beaa1c62d2c385ae6b33a441f
omarfurkan <omarfurkan92@gmail.com>
2023-07-23 11:42:22 +0600

    dark mode context apply done

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthContextProvider } from './context/AuthContext';
+import { AuthContextProvider } from './context/AuthContext.js';
+import { DarkModeContextProvider } from './context/darkModContext.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <DarkModeContextProvider>
+        <App />
+      </DarkModeContextProvider>
     </AuthContextProvider>
 
   </React.StrictMode>
